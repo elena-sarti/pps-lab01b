8f398c85c2fe0ecc441d012f1425531ddc3d655b
Elena Sarti <elena.sarti8@studio.unibo.it>
2026-03-04 12:58:14 +0100

    added javadoc for Position interface methods

--- src/main/java/it/unibo/pps/e2/Position.java
+++ src/main/java/it/unibo/pps/e2/Position.java
@@ -2,9 +2,21 @@ package it.unibo.pps.e2;
 
 public interface Position {
 
+     /**
+     * @param position
+     * @return whether the position of an object is valid
+     */
     boolean validatePosition(Pair<Integer,Integer> position);
 
+    /**
+     * @param coordinate
+     * @return whether a coordinate of a position is valid
+     */
     boolean validateCoordinate(int coordinate);
 
+    /**
+     *
+     * @return a new position with valid coordinates
+     */
     Pair<Integer,Integer> randomEmptyPosition();
 }
